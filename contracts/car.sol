22845e7e01adbd906fa802c37aaad817a82100b9
m <m7-660@outlook.com>
2020-07-15 11:22:49 +0300

    First version

---
+++ contracts/car.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+import "./Adoption.sol";
+contract Car is Adoption {
+
+    string public Type;
+    string public size;
+    uint public duration;
+    uint public timesdriv;
+
+    function Carr(string memory _Type, string memory _size, uint _duration) public {
+        Type = _Type;
+        size = _size;
+        duration = _duration;
+        timesdriv = 0;
+    }
+
+    function Drivecar() public onlyRenter whenRented returns(bool){
+        timesdriv++;
+        return true;
+    }
+}
